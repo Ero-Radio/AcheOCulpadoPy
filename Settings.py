fba48e84dad54f3eba99dca5c5b6e9644c136c91
Emil Freme <pedrofreme@MacBook-Pro.local>
2018-05-05 20:51:47 -0300

    Settings

--- Settings.py
+++ Settings.py
@@ -1,5 +1,10 @@
+import json
 def load_settings():
-    ...
+    settings = open("settings.json", "r", encoding="utf8")
+    settings = json.loads(settings.read())
+    sKeyMap = settings["keymap"]
+    return settings["keymap_options"][sKeyMap]
+
 
 def save_settings():
     ...
