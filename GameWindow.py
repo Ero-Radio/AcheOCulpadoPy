9564a0cae647fe70a47200406851005aec2dcf10
Pedro Freme <pedrofreme@MacBook-Pro.local>
2018-05-05 20:16:15 -0300

    Gitignore & first modules

---
+++ GameWindow.py
@@ -0,0 +1,6 @@
+import arcade
+class GameWindow(arcade.Window):
+
+    def __init__(self):
+        super().__init__(800, 600, "Ache o Culpado")
+
---
+++ main.py
@@ -0,0 +1,8 @@
+from GameWindow import *
+import arcade
+def main():
+    w = GameWindow()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
