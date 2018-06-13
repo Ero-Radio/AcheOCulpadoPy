2b80bff3364759e29c364ef2e5216a44d6281333
Eric Santos <ericsantosbr2@gmail.com>
2018-06-13 16:56:56 -0300

    Updated time counter

--- HUD.py
+++ HUD.py
@@ -4,11 +4,20 @@ import time
 startTime = 0
 currentTime = 0
 
-def draw():
-	timeString = "{0:02}:{1:02}:{2:02}".format(int(currentTime/360),
-	 int(currentTime/60), currentTime%60)
-	print(currentTime)
-	arcade.draw_text(timeString, 400, 500, (255, 255, 255), 40, align="center",anchor_x="center", anchor_y="center")
+def new():
+	return HUD()
 
-def update():
-	currentTime = time.time()-startTime
+class HUD:
+	def __init__(self):
+		self.currentTime = 0
+		self.startTime = 0
+
+	def draw(self):
+		self.timeString = "{0:02}:{1:02}:{2:02}".format(int(self.currentTime/360),
+		int(self.currentTime/60), self.currentTime%60)
+		print(self.currentTime)
+		arcade.draw_text(self.timeString, 400, 500, (255, 255, 255), 40, align="center",anchor_x="center", anchor_y="center")
+
+	def update(self):
+		self.currentTime += 1
+		# currentTime = time.time()-startTime
