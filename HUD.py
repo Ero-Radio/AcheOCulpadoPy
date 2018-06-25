352a4ed5a301f6c254e3e731c57a338920cf9a42
ericsantosbr <ericsantosbr2@gmail.com>
2018-06-25 19:54:45 -0300

    Updated inventory object read

--- HUD.py
+++ HUD.py
@@ -15,7 +15,7 @@ class HUD:
 	def draw(self):
 		self.timeString = "{0:02}:{1:02}:{2:02}".format(int(self.currentTime/360),
 		int(self.currentTime/60), self.currentTime%60)
-		print(self.currentTime)
+		# print(self.currentTime)
 		arcade.draw_text(self.timeString, 400, 500, (255, 255, 255), 40, align="center",anchor_x="center", anchor_y="center")
 
 	def update(self):
